540a4ef034bcd4c34ca092f83646e616910e121a
Jatin Kapur <jkapur.work@gmail.com>
2019-09-19 10:55:58 -0500

    update console.log and input statements for todos

--- to-do.js
+++ to-do.js
@@ -9,4 +9,15 @@ var todos = ["Buy New Turtle"];
 
 var input = prompt("what would you like to do? "); 
 
-if(input === "list")
+if(input === "list"){ 
+    console.log(todos); 
+} else if(input === "new") { 
+    //ask for new todo 
+    var newTodo = prompt("Enter a new Todo"); 
+    //add a new Todo 
+    todos.push(newTodo); 
+
+}
+
+
+console.log("connected"); 
